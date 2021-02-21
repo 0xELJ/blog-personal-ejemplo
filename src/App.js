9bdd41c817cba862ba73e42de19fd3272e2f228d
Enrique López Jiménez <loj.enrique@gmail.com>
2021-02-20 18:29:11 -0600

    cambios a los archivos index.js y App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Navbar extends Component {
+  getSpanStyles() {
+    return { fontWeight: "bold", fontSize: "32px" };
+  }
+
+  render() {
+    const url = "https://styles.redditmedia.com/t5_2su6s/styles/communityIcon_4g1uo0kd87c61.png";
+    const firstLink = "Inicio";
+
+    return (  
+      <div>
+        {/* HTML <img 
+          src="https://styles.redditmedia.com/t5_2su6s/styles/communityIcon_4g1uo0kd87c61.png" 
+          alt="Logo" 
+          style="width: 40px;"
+        > */}
+        {/* JSX <img /> */}
+        <img 
+          src={url} 
+          alt="Logo" 
+          // style="width: 40px;"
+          style={{ width: "40px" }}
+        />
+
+        {/* <span style="font-weight: bold; font-size: 32px;">Blog</span> */}
+        <span style={this.getSpanStyles()}>{5 > 8 ? "Blog" : "Web"}</span>
+
+        {/* <nav class="navbar"> */}
+        <nav className="navbar">
+          <ul>
+            <li>{firstLink}</li>
+            <li>Acerca</li>
+            <li>Publicaciones</li>
+            <li>Contacto</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
-export default App;
+class MainSection extends Component {
+  render() {
+    return <div>MainSection</div>;
+  }
+}
+
+class PostList extends Component {
+  render() {
+    return <Post />;
+  }
+}
+
+class Post extends Component {
+  render() {
+    return <div>Post</div>;
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return <div>Footer</div>;
+  }
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <MainSection />
+        <PostList />
+        <Footer />
+      </div>
+    );
+  }
+}
