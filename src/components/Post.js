f4f4d23b88f64604319a48f7bec80c5a5dbc6e9a
Enrique López Jiménez <loj.enrique@gmail.com>
2021-03-15 18:40:58 -0600

    agrega la estructura del componente Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 export default class Post extends Component {
   render() {
-    return <div>Post</div>;
+    return (
+      <div className="post">
+        <h1>{this.props.title}</h1>
+        <p>
+          {this.props.content}
+        </p>
+        <button onClick={this.props.onPress}>Leer más</button>
+      </div>
+    );
   }
 }
