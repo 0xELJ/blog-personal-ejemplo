6e05dd17392e4e4c1f22794b040d4404bf389a30
Enrique López Jiménez <loj.enrique@gmail.com>
2021-03-16 20:47:08 -0600

    maquetado de pie de página

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import Logo from "./Logo";
 
 export default class Footer extends Component {
   render() {
-    return <div>Footer</div>;
+    return (
+      <div className="container">
+        <div style={{ width: "50%", display: "inline-block" }}>
+          <Logo />
+        </div>
+        <div style={{ width: "50%", display: "inline-block" }}>
+          <p className="text-right">Aquí van los derechos de autor de mi blog personal.</p>
+        </div>
+      </div>
+    );
   }
 }
